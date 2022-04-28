42e023d936edef2f3b6152dbba67521794456593
Rodrigo Pita <rodrigopita@dcc.ufrj.br>
2022-04-27 23:54:47 -0300

    Código Finalizado

--- Módulo_2/Lab_2/cliente.py
+++ Módulo_2/Lab_2/cliente.py
@@ -1,1 +1,32 @@
+# cliente
 
+import socket
+
+HOST = 'localhost' # maquina onde esta o par passivo
+PORTA = 5000        # porta que o par passivo esta escutando
+
+# cria socket
+sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM 
+
+# conecta-se com o par passivo
+sock.connect((HOST, PORTA)) 
+
+while True:
+    # define a string para envio da mensagem ou codigo de parada
+    aux = input('Digite a mensagem: ')
+
+    # codigo de parada
+    if aux == 'stop': break
+    
+    # envia uma mensagem para o par conectado
+    sock.send(bytes(aux, 'utf-8'))
+    
+    #espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
+    msg = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
+
+    # imprime a mensagem recebida
+    print(str(msg,  encoding='utf-8'))
+    if str(msg,  encoding='utf-8') == 'FileNotFoundError': break
+
+# encerra a conexao
+sock.close() 
--- Módulo_2/Lab_2/servidor.py
+++ Módulo_2/Lab_2/servidor.py
@@ -1,9 +1,45 @@
-# Exemplo basico socket (lado passivo)
-
+# servidor
 import socket
 
 HOST = ''    # '' possibilita acessar qualquer endereco alcancavel da maquina local
 PORTA = 5000  # porta onde chegarao as mensagens para essa aplicacao
+
+def trataMsg(m):
+    '''Funcao que recebe uma string m, tenta abrir o arquivo com o nome da string 
+    e retorna uma string s com as 5 palavras de maior frequencia no arquivo'''
+    D = {} # dicionario auxiliar
+    s = '' # string de retorno
+    try:
+        for i in open(m):
+            for j in i.strip() \
+                    .lower() \
+                    .replace(':', '') \
+                    .replace('.', '') \
+                    .replace(',', '') \
+                    .split():
+                if j in D:
+                    D[j] += 1
+                else:
+                    D[j] = 1    
+        D_items = list(D.items()) # lista das tuplas (chave, valor) do dicionario
+        L = [('', 0), ('', 0), ('', 0), ('', 0), ('', 0)]
+        for k in D_items:
+            if k[1] > L[0][1]:
+                L[0] = k
+            elif k[1] > L[1][1]:
+                L[1] = k
+            elif k[1] > L[2][1]:
+                L[2] = k
+            elif k[1] > L[3][1]:
+                L[3] = k
+            elif k[1] > L[4][1]:
+                L[4] = k
+        for i in L:
+            s += (i[0] + ': ' + str(i[1]) + '\n')
+    except:
+        raise FileNotFoundError
+        s = 'FileNotFoundError'
+    return s
 
 # cria um socket para comunicacao
 sock = socket.socket() # valores default: socket.AF_INET, socket.SOCK_STREAM  
@@ -19,40 +55,15 @@ novoSock, endereco = sock.accept() # retorna um novo socket e o endereco do par 
 print ('Conectado com: ', endereco)
 
 while True:
-	# depois de conectar-se, espera uma mensagem (chamada pode ser BLOQUEANTE))
-	msg = novoSock.recv(1024) # argumento indica a qtde maxima de dados
-	if not msg: break 
-	else:
-            D = {}
-            try:
-                for i in open(str(msg, encoding='utf-8')):
-                    for j in i.strip() \
-                        .lower() \
-                        .replace(':', '') \
-                        .replace('.', '') \
-                        .replace(',', '') \
-                        .split():
-                        if j in D:
-                                D[j] += 1
-                        else:
-                                D[j] = 1    
-            except:
-                raise FileNotFoundError
-            D_items = list(D.items())
-            L = [('', 0), ('', 0), ('', 0), ('', 0), ('', 0)]
-            for k in D_items:
-                if k[1] > L[0][1]:
-                        L[0] = k
-                elif k[1] > L[1][1]:
-                        L[1] = k
-                elif k[1] > L[2][1]:
-                        L[2] = k
-                elif k[1] > L[3][1]:
-                        L[3] = k
-                elif k[1] > L[4][1]:
-                        L[4] = k
-	# envia mensagem de resposta
-	novoSock.send(msg) 
+        # depois de conectar-se, espera uma mensagem (chamada pode ser BLOQUEANTE))
+        msg = novoSock.recv(1024) # argumento indica a qtde maxima de dados
+        if not msg: break 
+        else:
+            aux = (str(msg,  encoding='utf-8'))
+            novaMsg = trataMsg(aux)
+            # envia mensagem de resposta
+            novoSock.send(bytes(novaMsg, 'utf-8'))
+            if novaMsg == 'FileNotFoundError': break
 
 # fecha o socket da conexao
 novoSock.close() 
