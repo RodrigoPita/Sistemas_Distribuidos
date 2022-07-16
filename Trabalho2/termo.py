bcf14a0b605591d53b2966f24e9569aa04d88777
Lorena Mamede <loremamede@gmail.com>
2022-07-16 13:39:35 -0300

    [Trabalho 2] Refatorando projeto
    Melhorando legibilidade do código

--- Trabalho2/termo.py
+++ Trabalho2/termo.py
@@ -10,12 +10,14 @@ from time import time
 from math import floor
 
 # nome do banco de palavras
-file_name = 'dictionary_2.txt'
-file_name_2 = 'display_dict.txt'
-file_name_3 = 'dictionary.txt'
+NO_ACCENT_DICT_2 = 'word_processing/result/no_accent_dictionary_2.txt'
+COMPLETE_DICT = 'words_dataset/complete_dictionary.txt'
+FILE_NAME_3 = 'words_dataset/no_accent_complete_dictionary.txt'
+
+MAX_ROUNDS = 6
 
 # lista com as palavras
-possible_words = []
+playable_words = []
 words = []
 words_display = []
 
@@ -26,14 +28,14 @@ alphabet = [ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 
 # abrindo o banco de palavras, para popular a lista
 
-for word in open( file_name ):
-    possible_words.append( word.replace( '\n', '' ) )
+for word in open( NO_ACCENT_DICT_2 ):
+    playable_words.append( word.replace( '\n', '' ) )
 
-for word in io.open( file_name_2, mode = 'r', encoding = 'utf-8' ):
+for word in io.open( COMPLETE_DICT, mode = 'r', encoding = 'utf-8' ):
     word = word.strip()
     words_display.append( word )
 
-for word in open( file_name_3 ):
+for word in open( FILE_NAME_3 ):
     words.append( word.replace( '\n', '' ) )
 
 def chooseWord( list_of_words:list = words ) -> str:
@@ -79,29 +81,47 @@ def colorLetterBG( l:str, token:str ) -> str:
     new_l = pallete[token] + l[0] + pallete['w']
     return new_l
 
-def wordAnalize( test_word:str, real_word:str ) -> list:
+def check_repetitions(position_colors, victory_word, repeated_letters):
+    for i in range( len( position_colors ) ):
+        if ( repetitionAnalize( victory_word, position_colors, repeated_letters, i ) ):
+            if ( len( position_colors[i] ) > 1 ):
+                if ( position_colors[i][1] == 'y' ):
+                    repeated_letters.remove( position_colors[i][0] )
+                    position_colors[i] = position_colors[i][0]
+
+    return repeated_letters, position_colors
+
+
+def analyze_letter_position(guess_letter, victory_letter, victory_word, repeated_letters):
+    if ( guess_letter == victory_letter ): 
+        return guess_letter + 'g' 
+    else:
+        guess_letter_occurences = victory_word.count( guess_letter )
+        if (repeated_letters.count( guess_letter ) < guess_letter_occurences ): 
+            return guess_letter + 'y'
+        else: 
+            return guess_letter
+    
+
+def analyzeWord( user_guess:str, victory_word:str ) -> list:
     '''Analisa quantas letras a palavra teste tem em comum com a palavra real
     e atrela um token nas letras que estiverem certas
     "g" se a letra pertencer a palavra e estiver na posicao certa
     "y" se a letra pertencer a palavra, mas estiver na posicao errada'''
     position_colors = [] # lista com as letras e seus respectivos tokens
     repeated_letters = [] # lista de letras repetidas que pertencem a palavra real
-    for i in range( len( test_word ) ):
-        if ( test_word[i] in real_word ): 
-            if ( test_word[i] == real_word[i] ): position_colors.append( test_word[i] + 'g' )
-            else:
-                aux = real_word.count( test_word[i] )
-                if ( repeated_letters.count( test_word[i] ) < aux ): position_colors.append( test_word[i] + 'y' )
-                else: position_colors.append( test_word[i] )
-            repeated_letters.append( test_word[i] )
-        else: position_colors.append( test_word[i] )
-    for i in range( len( position_colors ) ):
-        if ( repetitionAnalize( real_word, position_colors, repeated_letters, i ) ):
-            if ( len( position_colors[i] ) > 1 ):
-                if ( position_colors[i][1] == 'y' ):
-                    repeated_letters.remove( position_colors[i][0] )
-                    position_colors[i] = position_colors[i][0]
+    for i in range( len( user_guess ) ):
+        if ( user_guess[i] in victory_word ): 
+            marked_letter = analyze_letter_position(user_guess[i], victory_word[i], victory_word, repeated_letters)
+            repeated_letters.append( user_guess[i] )
+        else: marked_letter = user_guess[i]
+
+        position_colors.append(marked_letter)
+
+    repeated_letters, position_colors = check_repetitions(position_colors, victory_word, repeated_letters)
+    
     return position_colors
+
 
 def repetitionAnalize( real_word:str, position_colors:list, repeated_letters:list, pos:int ) -> bool:
     '''Analisa se alguma letra foi contada mais do que devia'''
@@ -110,11 +130,11 @@ def repetitionAnalize( real_word:str, position_colors:list, repeated_letters:lis
             or position_colors.count( position_colors[pos][0] + 'y' ) + 
             position_colors.count( position_colors[pos][0] + 'g' ) == repeated_letters.count( position_colors[pos][0] ) ) )
 
-def finalMessage( test:list , w:str, w_display:str , round:int, start_time:float ) -> None:
+def finalMessage( guess:list , chosen_word:str, w_display:str , round:int, start_time:float ) -> None:
     '''Imprime a mensagem final do jogo'''
     elapsed_time = stopWatch( start_time, time() )
     formatted_time = formatTime( elapsed_time )
-    if ( test == w ): print( f'\nParabéns, você acertou em {round} tentativas no período de {formatted_time} minutos')
+    if ( guess == chosen_word ): print( f'\nParabéns, você acertou em {round} tentativas no período de {formatted_time} minutos')
     else: print( f'\nA palavra era {w_display}, mais sorte da próxima vez')
 
 def reduceAlphabet( letters:list, used_letters:list ) -> list:
@@ -146,28 +166,35 @@ def formatTime( t:float ) -> str:
     seconds = round( frac * 60 )
     return str( minutes ) + ':' + str( seconds )
 
+def get_displayable_format(chosen_word):
+    index_word = words.index( chosen_word )
+    return words_display[index_word]
+
+def get_player_guess(round):
+    guess = input( f'\nTentativa {round}: ').upper()
+    while ( guess not in words ): guess = input( f'Tentativa {round}: ').upper()
+    return guess
+
+def process_rounds_guess(chosen_word):
+    attempts = []
+    possible_letters = [] + alphabet
+    for round in range( 1, MAX_ROUNDS+1 ):
+        printAlphabet( possible_letters )
+        guess = get_player_guess(round)
+        if ( round == 1 ): start = time()
+        result = analyzeWord( guess, chosen_word )
+        possible_letters = reduceAlphabet( possible_letters, result )
+        attempts.append( result )
+        displayAttempts( chosen_word, attempts, round )
+        if ( guess == chosen_word ): break
+    return round, start, guess
+
 def beginGame():
     '''Da inicio ao jogo'''
-    chosen_word = chooseWord( possible_words )
-    index_word = words.index( chosen_word )
-    chosen_word_display = words_display[index_word]
-    possible_letters = [] + alphabet
-    attempts = []
-    final_round = 6
-    for round in range( 1, 7 ):
-        printAlphabet( possible_letters )
-        test = input( f'\nTentativa {round}: ').upper()
-        while ( test not in words ): test = input( f'Tentativa {round}: ').upper()
-        if ( round == 1 ): start = time()
-        aux = wordAnalize( test, chosen_word )
-        possible_letters = reduceAlphabet( possible_letters, aux )
-        attempts.append( aux )
-        if ( test == chosen_word ):
-            final_round = round
-            displayAttempts( chosen_word, attempts )
-            break
-        displayAttempts( chosen_word, attempts, round )
-    finalMessage( test, chosen_word ,chosen_word_display, final_round, start )
+    chosen_word = chooseWord( playable_words )
+    chosen_word_display = get_displayable_format(chosen_word)
+    final_round, start, guess = process_rounds_guess(chosen_word)
+    finalMessage( guess, chosen_word ,chosen_word_display, final_round, start )
 
 def main():
     beginGame()
